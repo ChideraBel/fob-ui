59607353163e64f6cda8d9ebd5ecf75aa2913cca
Ola Akinola <ola.akinola@zoro.com>
2024-02-18 15:33:49 -0600

    >:(

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import LoginScreen from './src/screens/LoginScreen';
+import React from 'react';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <LoginScreen/>
+    </SafeAreaView>
   );
 }
 
---
+++ src/screens/LoginScreen/LoginScreen.tsx
@@ -0,0 +1,26 @@
+import { View, Image, StyleSheet, useWindowDimensions } from "react-native";
+import Logo from '../../assets/japaboat.png';
+
+const LoginScreen = () => {
+  const {height} = useWindowDimensions();
+  return (
+    <View>
+      <Image source={Logo} style={[styles.logo,{height: height* 0.3}]}/>
+      hello
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  root: {
+    alignItems: 'center',
+    padding: 40,
+  },
+  logo:{
+    width: '70%',
+    maxWidth: 300,
+    maxHeight:200,
+  }
+})
+
+export default LoginScreen;
